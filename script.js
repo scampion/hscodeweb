bcaff3b9fa58d67b19bc44e5d1b230469bb2c756
Sebastien Campion <sebastien.campion@gmail.com>
2021-01-17 20:11:59 +0100

    update

--- script.js
+++ script.js
@@ -40,16 +40,22 @@ for (var i = 0; i < input_dim; i += 1) {
     }
 }
 
+function argMax(array) {
+  return [].map.call(array, (x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+}
 
 var model = load_model();
 var input = tf.tensor(X, [1, 321]);
+var prediction;
 
 model.then(function (res) {
     console.log(X);
     console.log(input);
-    const prediction = res.predict(input);
+    prediction = res.predict(input);
     console.log("Prediction : ")
     console.log(prediction);
+    var c1 =  argMax(prediction.dataSync())
+    
 
 }, function (err) {
     console.log(err);
