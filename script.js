829ca6d74e8956d3111cfff695cfea8f482212aa
Sebastien Campion <sebastien.campion@gmail.com>
2021-01-17 19:05:39 +0100

    update

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-
 function getJSON(url) {
-    var resp ;
-    var xmlHttp ;
+    var resp;
+    var xmlHttp;
 
-    resp  = '' ;
+    resp = '';
     xmlHttp = new XMLHttpRequest();
 
-    if(xmlHttp != null)
-    {
-        xmlHttp.open( "GET", url, false );
-        xmlHttp.send( null );
+    if (xmlHttp != null) {
+        xmlHttp.open("GET", url, false);
+        xmlHttp.send(null);
         resp = xmlHttp.responseText;
     }
 
-    return resp ;
+    return resp;
 }
 
-async function load_model(){
- const modelURL = 'http://localhost:8000/tfjs/model.json';
- const model = await tf.loadGraphModel(modelURL)
- //const model = await tf.loadLayersModel(modelURL);
- return model;
+async function load_model() {
+    const modelURL = './tfjs/model.json';
+    const model = await tf.loadLayersModel(modelURL)
+    //const model = await tf.loadLayersModel(modelURL);
+    return model;
 }
 
 var vocab = JSON.parse(getJSON('./word_dict.json'));
@@ -29,21 +27,30 @@ var query = "jeans for woman";
 q = query.toLowerCase()
 var words = q.split(" ");
 var input_dim = 321;
-for (var i = 0; i < input_dim;  i += 1) {
- if ( i < words.length){
-  var code = vocab[words[i]];
-  if (code) {
-   X.push(code)
-  } else {
-   X.push(0)
-  }
- } else {
-  X.push(0)
- }
+for (var i = 0; i < input_dim; i += 1) {
+    if (i < words.length) {
+        var code = vocab[words[i]];
+        if (code) {
+            X.push(code)
+        } else {
+            X.push(0)
+        }
+    } else {
+        X.push(0)
+    }
 }
-console.log(X);
 
 
 var model = load_model();
-var input = convertInputToTensor(q);
-model.predict(input);
+var input = tf.tensor(X, [1, 321]);
+
+model.then(function (res) {
+    console.log(X);
+    console.log(input);
+    const prediction = res.predict(input);
+    console.log("Prediction : ")
+    console.log(prediction);
+
+}, function (err) {
+    console.log(err);
+});
